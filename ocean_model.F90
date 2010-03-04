e2872cab800f053c553dcd113f8b6c919c2ec1c6
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2010-03-04 00:00:00 -0500

    Import of riga.

--- ocean_model.F90
+++ ocean_model.F90
@@ -1,18 +1,26 @@
 module ocean_model_mod
 
-use mpp_domains_mod, only: domain2d
+use   mpp_domains_mod, only: domain2d, mpp_define_layout, mpp_define_domains, mpp_get_compute_domain
 
-use fms_mod, only: error_mesg, FATAL
+use           fms_mod, only: error_mesg, FATAL, write_version_number, mpp_npes, read_data
+use           fms_mod, only: field_size, field_exist, get_mosaic_tile_grid
 
-use coupler_types_mod,only: coupler_2d_bc_type
+use  time_manager_mod, only: time_type
+
+use coupler_types_mod, only: coupler_2d_bc_type
+
+use        mosaic_mod, only: get_mosaic_ntiles, get_mosaic_grid_sizes, get_mosaic_xgrid
+use        mosaic_mod, only: get_mosaic_xgrid_size, calc_mosaic_grid_area
+
+use     constants_mod, only: PI, RADIUS
 
 implicit none
 private
 
-public :: ocean_model_init, ocean_model_end, update_ocean_model, ocean_data_type, &
-          ice_ocean_boundary_type, ocean_grids_type, read_ice_ocean_boundary, &
-          write_ice_ocean_boundary, init_default_ice_ocean_boundary, &
-          ocean_model_flux_init, ocean_model_init_sfc, ocean_stock_pe, ocean_model_restart
+public :: ocean_model_init, ocean_model_end, update_ocean_model, &
+          ice_ocean_boundary_type, ocean_grids_type, &
+          ocean_model_flux_init, ocean_model_init_sfc, &
+          ocean_stock_pe, ocean_model_restart
 
 public    ocean_model_data_get
 interface ocean_model_data_get
@@ -30,9 +38,8 @@ type ice_ocean_boundary_type
                                    salt_flux =>NULL(), &
                                    lw_flux =>NULL(), &
                                    sw_flux =>NULL(), &
-                                   sw_flux_vis =>NULL(), &
-                                   sw_flux_dir =>NULL(), &
-                                   sw_flux_dif =>NULL(), &
+                                   sw_flux_nir_dir =>NULL(), &
+                                   sw_flux_nir_dif =>NULL(), &
                                    sw_flux_vis_dir =>NULL(), &
                                    sw_flux_vis_dif =>NULL(), &
                                    lprec =>NULL(), &
@@ -57,7 +64,7 @@ end type ice_ocean_boundary_type
 
 !-----------------------------------------------------------------------
 
-type ocean_public_type
+type, public :: ocean_public_type
    type (domain2d)               :: Domain
    type (ocean_grids_type)       :: Global, Data
    real, pointer, dimension(:,:) :: t_surf =>NULL() , &
@@ -68,7 +75,7 @@ type ocean_public_type
                                     area   =>NULL() , &
                                     sea_lev=>NULL()
    logical, pointer, dimension(:,:) :: maskmap =>NULL()
-   logical :: pe
+   logical :: is_ocean_pe = .false.
    integer, pointer :: pelist(:) =>NULL()
    integer, dimension(3)            :: axes    
    type(coupler_2d_bc_type)         :: fields
@@ -83,21 +90,21 @@ end type ocean_public_type
 
 !-----------------------------------------------------------------------
 
-   character(len=128) :: version = '$Id: ocean_model.F90,v 17.0 2009/07/21 03:17:55 fms Exp $'
-   character(len=128) :: tagname = '$Name: quebec_200910 $'
+   character(len=128) :: version = '$Id: ocean_model.F90,v 18.0 2010/03/02 23:54:27 fms Exp $'
+   character(len=128) :: tagname = '$Name: riga $'
 
 contains
 
 !#######################################################################
 
- subroutine update_ocean_model (Ice_boundary, Ocean, Ocean_sfc, &
+ subroutine update_ocean_model (Ice_boundary, Ocean_state, Ocean_sfc, &
        time_start_update, Ocean_coupling_time_step)
 
- type (ice_ocean_boundary_type), intent(in) :: Ice_boundary
- type(ocean_state_type),   pointer       :: Ocean_state
- type (ocean_public_type), intent(inout) :: Ocean
- type(time_type), intent(in)             :: time_start_update
- type(time_type), intent(in)             :: Ocean_coupling_time_step
+ type(ice_ocean_boundary_type), intent(in)    :: Ice_boundary
+ type(ocean_state_type),        pointer       :: Ocean_state
+ type(ocean_public_type),       intent(inout) :: Ocean_sfc
+ type(time_type), intent(in)                  :: time_start_update
+ type(time_type), intent(in)                  :: Ocean_coupling_time_step
 
  call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
 
@@ -107,11 +114,159 @@ contains
 
  subroutine ocean_model_init (Ocean, Ocean_state, Time_init, Time)
 
- type (ocean_public_type), intent(inout) :: Ocean
- type(ocean_state_type),      pointer    :: Ocean_state
- type (time_type), intent(in) :: Time_init, Time, Time_step
+ type(ocean_public_type), intent(inout) :: Ocean
+ type(ocean_state_type),  pointer       :: Ocean_state
+ type(time_type), intent(in) :: Time_init, Time
 
- call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
+ real,    allocatable, dimension(:)     :: xgrid_area
+ real,    allocatable, dimension(:,:)   :: geo_lonv, geo_latv, rmask, geo_lont, geo_latt, tmpx, tmpy, garea
+ real,    allocatable, dimension(:,:,:) :: x_vert_t, y_vert_t
+ integer, allocatable, dimension(:)     :: i1, j1, i2, j2
+ integer                                :: i, j, ntiles, nfile_axo, nxgrid, n, m, grid_version, nlon, nlat
+ integer                                :: isd, ied, jsd, jed
+ integer                                :: layout(2), siz(4), nx(1), ny(1)
+ character(len=256)                     :: grid_file = "INPUT/grid_spec.nc"
+ character(len=256)                     :: ocean_mosaic, tile_file, ocn_mosaic_file, axo_file
+
+ call write_version_number(version, tagname)
+
+
+    if(field_exist(grid_file, 'geolon_t')) then
+       grid_version = 0
+       call field_size( grid_file, 'geolon_t', siz)
+       nlon = siz(1)
+       nlat = siz(2)
+    else if(field_exist(grid_file, 'x_T')) then
+       grid_version = 1
+       call field_size( grid_file, 'x_T', siz)
+       nlon = siz(1)
+       nlat = siz(2)
+    else if(field_exist(grid_file, 'ocn_mosaic_file') ) then ! read from mosaic file
+       grid_version = 2
+       call read_data(grid_file, "ocn_mosaic_file", ocean_mosaic)
+       ocean_mosaic = "INPUT/"//trim(ocean_mosaic)
+       ntiles = get_mosaic_ntiles(ocean_mosaic)
+       if(ntiles .NE. 1) call error_mesg('ocean_model_init', ' ntiles should be 1 for ocean mosaic, contact developer', FATAL)
+       call get_mosaic_grid_sizes( ocean_mosaic, nx, ny)
+       nlon = nx(1)
+       nlat = ny(1)
+    else
+       call error_mesg('ocean_model_init','x_T, geolon_t, ocn_mosaic_file does not exist in file '//trim(grid_file), FATAL )
+    end if
+
+    allocate (Ocean%Global%lon_bnd (nlon+1),     &
+              Ocean%Global%lat_bnd (nlat+1),     &
+              Ocean%Global%lon     (nlon, nlat), &
+              Ocean%Global%lat     (nlon, nlat), &
+              Ocean%Global%mask    (nlon, nlat))
+
+    allocate (rmask(nlon,nlat), geo_lont(nlon,nlat), geo_latt(nlon,nlat), &
+              geo_lonv(1:nlon+1,1:nlat+1), geo_latv(1:nlon+1,1:nlat+1) )
+
+    layout = (/0,0/)
+    call mpp_define_layout( (/1,nlon,1,nlat/), mpp_npes(), layout)
+    call mpp_define_domains ( (/1,nlon,1,nlat/), layout, Ocean%Domain, name='NULL Ocean')
+
+    select case (grid_version)
+    case(0)
+       call read_data(grid_file, "geolon_t",      geo_lont, no_domain=.TRUE. )
+       call read_data(grid_file, "geolat_t",      geo_latt, no_domain=.TRUE. )
+       call read_data(grid_file, "geolon_vert_t", geo_lonv, no_domain=.TRUE. )
+       call read_data(grid_file, "geolat_vert_t", geo_latv, no_domain=.TRUE. )
+       call read_data(grid_file, "wet",      rmask,     no_domain=.TRUE.)
+    case(1)
+       allocate (x_vert_t(nlon,nlat,4), y_vert_t(nlon,nlat,4) )
+       call read_data(grid_file, "x_T", geo_lont, no_domain=.TRUE. )
+       call read_data(grid_file, "y_T", geo_latt, no_domain=.TRUE. )
+       call read_data(grid_file, "x_vert_T", x_vert_t, no_domain=.TRUE.)
+       call read_data(grid_file, "y_vert_T", y_vert_t, no_domain=.TRUE. )
+       geo_lonv(1:nlon,1:nlat) = x_vert_t(1:nlon,1:nlat,1)
+       geo_lonv(nlon+1,1:nlat) = x_vert_t(nlon,1:nlat,2)
+       geo_lonv(1:nlon,nlat+1) = x_vert_t(1:nlon,nlat,4)
+       geo_lonv(nlon+1,nlat+1) = x_vert_t(nlon,nlat,3)
+       geo_latv(1:nlon,1:nlat) = y_vert_t(1:nlon,1:nlat,1)
+       geo_latv(nlon+1,1:nlat) = y_vert_t(nlon,1:nlat,2)
+       geo_latv(1:nlon,nlat+1) = y_vert_t(1:nlon,nlat,4)
+       geo_latv(nlon+1,nlat+1) = y_vert_t(nlon,nlat,3)
+       deallocate(x_vert_t, y_vert_t)
+       call read_data(grid_file, "wet",      rmask,     no_domain=.TRUE.)
+    case(2)
+       call get_mosaic_tile_grid(tile_file, ocean_mosaic, Ocean%Domain )
+       allocate(tmpx(2*nlon+1, 2*nlat+1), tmpy(2*nlon+1, 2*nlat+1) )
+       allocate(garea(nlon, nlat))
+       call read_data(tile_file, "x", tmpx, no_domain=.TRUE.)
+       call read_data(tile_file, "y", tmpy, no_domain=.TRUE.)
+       do j = 1, nlat
+          do i = 1, nlon
+             geo_lont(i,j) = tmpx(i*2,j*2)
+             geo_latt(i,j) = tmpy(i*2,j*2)
+          end do
+       end do
+       do j = 1, nlat+1
+          do i = 1, nlon+1
+             geo_lonv(i,j) = tmpx(i*2-1,j*2-1)
+             geo_latv(i,j) = tmpy(i*2-1,j*2-1)
+          end do
+       end do
+
+       call calc_mosaic_grid_area(geo_lonv*pi/180., geo_latv*pi/180., garea )
+       garea = garea/(4*PI*RADIUS*RADIUS)  ! scale the earth are to be 1
+       call field_size(grid_file, "aXo_file", siz)
+       nfile_axo = siz(2)
+       rmask = 0.0
+       do n = 1, nfile_axo
+          call read_data(grid_file, "aXo_file", axo_file, level=n)
+          axo_file = 'INPUT/'//trim(axo_file)
+          nxgrid = get_mosaic_xgrid_size(axo_file)
+          allocate(i1(nxgrid), j1(nxgrid), i2(nxgrid), j2(nxgrid), xgrid_area(nxgrid))
+          call get_mosaic_xgrid(aXo_file, i1, j1, i2, j2, xgrid_area)
+          do m = 1, nxgrid
+             i = i2(m); j = j2(m)
+             rmask(i,j) = rmask(i,j) + xgrid_area(m)
+          end do
+          deallocate(i1, j1, i2, j2, xgrid_area)
+       end do
+       rmask = rmask/garea
+
+       deallocate(tmpx, tmpy, garea)
+    end select
+
+   Ocean%Global%mask = .false.
+   where (rmask > 0) Ocean%Global%mask = .true.
+   Ocean%Global%lon_bnd = geo_lonv(:,1)*pi/180.
+   Ocean%Global%lat_bnd = geo_latv(1,:)*pi/180.
+   Ocean%Global%lon = geo_lont*pi/180.
+   Ocean%Global%lat = geo_latt*pi/180.
+   deallocate(geo_lonv, geo_latv, geo_lont, geo_latt, rmask)
+
+   call mpp_get_compute_domain ( Ocean%Domain, isd, ied, jsd, jed )
+   allocate ( Ocean%Data%lon_bnd (isd:ied+1),      &
+              Ocean%Data%lat_bnd (jsd:jed+1),      &
+              Ocean%Data%lon (isd:ied, jsd:jed),   &
+              Ocean%Data%lat (isd:ied, jsd:jed),   &
+              Ocean%Data%mask(isd:ied,jsd:jed))
+
+   Ocean%Data%lon_bnd = Ocean%Global%lon_bnd(isd:ied+1)
+   Ocean%Data%lat_bnd = Ocean%Global%lat_bnd(jsd:jed+1)
+   Ocean%Data%lon     = Ocean%Global%lon(isd:ied,jsd:jed)
+   Ocean%Data%lat     = Ocean%Global%lat(isd:ied,jsd:jed)
+   Ocean%Data%mask    = .FALSE.
+
+   allocate ( Ocean%t_surf (isd:ied,jsd:jed), &
+              Ocean%u_surf (isd:ied,jsd:jed), &
+              Ocean%v_surf (isd:ied,jsd:jed), &
+              Ocean%frazil (isd:ied,jsd:jed), &
+              Ocean%area   (isd:ied,jsd:jed), &
+              Ocean%s_surf (isd:ied,jsd:jed), &
+              Ocean%sea_lev(isd:ied,jsd:jed))
+
+    Ocean%t_surf  = 280.
+    Ocean%u_surf  = 0.0
+    Ocean%v_surf  = 0.0
+    Ocean%frazil  = 0.0
+    Ocean%area    = 1.0
+    Ocean%s_surf  = 0.0
+    Ocean%sea_lev = 0.0
 
  end subroutine ocean_model_init
 
@@ -122,6 +277,8 @@ contains
   type(ocean_state_type),            pointer    :: Ocean_state
   type(time_type),                   intent(in) :: Time_in
   type(ocean_public_type), optional, intent(in) :: Ocean
+
+     ! dummy routine
 
   end subroutine ocean_model_end
 
@@ -150,51 +307,24 @@ contains
   type(ocean_state_type),  pointer    :: Ocean_state    
   type(ocean_public_type), intent(in) :: Ocean
 
-  call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
+  
 
   end subroutine ocean_model_init_sfc
 !#######################################################################
-  subroutine ocean_model_flux_init
+  subroutine ocean_model_flux_init(Ocean_state)
+  type(ocean_state_type), pointer :: Ocean_state
 
-  call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
+ 
 
   end subroutine ocean_model_flux_init
 !#######################################################################
-  subroutine read_ice_ocean_boundary(file_name,iob,Ocean)
-
-  character(LEN=*),             intent(IN)    :: file_name  
-  type(ice_ocean_boundary_type),intent(INOUT) :: iob
-  type(ocean_public_type),        intent(IN)    :: Ocean
-
-  call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
-
-  end subroutine read_ice_ocean_boundary
-!#######################################################################
-  subroutine write_ice_ocean_boundary(file_name,iob,Ocean)
-
-  character(LEN=*),             intent(IN) :: file_name  
-  type(ice_ocean_boundary_type),intent(IN) :: iob
-  type(ocean_public_type),        intent(IN) :: Ocean
-
-  call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
-
-  end subroutine write_ice_ocean_boundary
-!#######################################################################
-  subroutine init_default_ice_ocean_boundary(iob)
-
-  type(ice_ocean_boundary_type),intent(INOUT) :: iob
-
-  call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
-
-  end subroutine init_default_ice_ocean_boundary
-!#######################################################################
   subroutine ocean_stock_pe(Ocean_state, index, value, time_index)
-  type(ocean_state_type), intent(in) :: Ocean_state
+  type(ocean_state_type), pointer    :: Ocean_state
   integer,               intent(in)  :: index
   real,                  intent(out) :: value
   integer, optional,     intent(in)  :: time_index
 
-  call error_mesg('ocean_model_mod', 'null ocean model should not be executed', FATAL )
+  value = 0.0
 
   end subroutine ocean_stock_pe
 !#######################################################################
