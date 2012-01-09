7e648691909603b7dc1da01e5ea765cb03a1c6f8
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2012-01-09 00:00:00 -0500

    Import of siena.

--- ocean_model.F90
+++ ocean_model.F90
@@ -91,8 +91,8 @@ end type ocean_public_type
 
 !-----------------------------------------------------------------------
 
-   character(len=128) :: version = '$Id: ocean_model.F90,v 18.0.2.1 2010/09/07 18:44:36 pjp Exp $'
-   character(len=128) :: tagname = '$Name: riga_201104 $'
+   character(len=128) :: version = '$Id: ocean_model.F90,v 19.0 2012/01/06 21:52:55 fms Exp $'
+   character(len=128) :: tagname = '$Name: siena $'
 
 contains
 
