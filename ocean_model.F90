7f5fabe1630e9f3473a9ad8af923c9588308ca34
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2013-12-20 00:00:00 -0500

    Import of tikal.

--- ocean_model.F90
+++ ocean_model.F90
@@ -92,8 +92,8 @@ end type ocean_public_type
 
 !-----------------------------------------------------------------------
 
-   character(len=128) :: version = '$Id: ocean_model.F90,v 19.0.4.1 2013/02/01 19:35:44 Peter.Phillipps Exp $'
-   character(len=128) :: tagname = '$Name: siena_201309 $'
+   character(len=128) :: version = '$Id: ocean_model.F90,v 20.0 2013/12/14 00:17:39 fms Exp $'
+   character(len=128) :: tagname = '$Name: tikal $'
 
 contains
 
